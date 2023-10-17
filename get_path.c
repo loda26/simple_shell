b0eb7b1121dd47a6719a4410269a444a94516be4
loda26 <mokhaled26498@gmail.com>
2023-10-17 07:47:48 -0700

    get the path of the command

--- get_path.c
+++ get_path.c
@@ -5,41 +5,42 @@
  * @command: input
  * Return: path or NULL
  */
+
 char *_getpath(char *command)
 {
-char *path_env, *cmd, *road;
-int i;
-struct stat st;
+	char *path_env, *cmd, *road;
+	int i;
+	struct stat st;
 
-for (i = 0; command[i] ; i++)
+	for (i = 0; command[i] ; i++)
 	{
-	if (command[i] == '/')
-	{
-	if (stat(command, &st) == 0)
-	return (strdup(command));
-	return (NULL);
+		if (command[i] == '/')
+		{
+			if (stat(command, &st) == 0)
+				return (strdup(command));
+		return (NULL);
+		}
 	}
-	}
-path_env = _getenv("PATH");
+	path_env = _getenv("PATH");
 	if (!path_env)
-	return (NULL);
+		return (NULL);
 	road = strtok(path_env, ":");
 	while (road)
 	{
-cmd = malloc(_strlen(road) + _strlen(command) + 2);
-	if (cmd)
-	{
-	_strcpy(cmd, road);
-	_strcat(cmd, "/");
-	_strcat(cmd, command);
-	if (stat(cmd, &st) == 0)
-	{
-	free(path_env);
-	return (cmd);
-	}
-	free(cmd);
-	road = strtok(NULL, ":");
-	}
+		cmd = malloc(_strlen(road) + _strlen(command) + 2);
+		if (cmd)
+		{
+			_strcpy(cmd, road);
+			_strcat(cmd, "/");
+			_strcat(cmd, command);
+			if (stat(cmd, &st) == 0)
+			{
+				free(path_env);
+				return (cmd);
+			}
+			free(cmd);
+			road = strtok(NULL, ":");
+		}
 	}
 	free(path_env);
 	return (NULL);
